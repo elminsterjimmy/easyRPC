564d695031989364e9746a56e410e842229df1ff
Jinglei.Gu <jinglei.gu@oracle.com>
2017-06-23 15:52:33 +0800

    data compressor interfaces.

--- easyRPC-common/src/main/java/com/elminster/easy/rpc/compressor/DataCompressor.java
+++ easyRPC-common/src/main/java/com/elminster/easy/rpc/compressor/DataCompressor.java
@@ -6,9 +6,36 @@ import java.io.OutputStream;
 
 import com.elminster.easy.rpc.exception.RpcException;
 
+/**
+ * The Data Compressor.
+ * 
+ * @author jinggu
+ * @version 1.0
+ */
 public interface DataCompressor {
 
-  public void deflate(Object obj, OutputStream out) throws IOException;
-  
-  public Object inflate(InputStream in) throws IOException, RpcException;
+  /**
+   * Deflate the data.
+   * 
+   * @param data
+   *          the data
+   * @param out
+   *          the output stream
+   * @throws IOException
+   *           on error
+   */
+  public void deflate(final Object data, final OutputStream out) throws IOException;
+
+  /**
+   * Inflate the data.
+   * 
+   * @param in
+   *          the input stream
+   * @return the data
+   * @throws IOException
+   *           on error
+   * @throws RpcException
+   *           on error
+   */
+  public Object inflate(final InputStream in) throws IOException, RpcException;
 }
--- easyRPC-common/src/main/java/com/elminster/easy/rpc/compressor/DataCompressorFactory.java
+++ easyRPC-common/src/main/java/com/elminster/easy/rpc/compressor/DataCompressorFactory.java
@@ -2,11 +2,39 @@ package com.elminster.easy.rpc.compressor;
 
 import com.elminster.easy.rpc.exception.RpcException;
 
+/**
+ * The Data Compressor Factory.
+ * 
+ * @author jinggu
+ * @version 1.0
+ */
 public interface DataCompressorFactory {
 
-  public DataCompressor getCompressor(int param) throws RpcException;
-  
-  public void addCompressor(int param, Class<? extends DataCompressor> compressorClass);
-  
+  /**
+   * Get the Data Compressor by type.
+   * 
+   * @param type
+   *          the Data Compressor type
+   * @return the Data Compressor
+   * @throws RpcException
+   *           on error
+   */
+  public DataCompressor getCompressor(final int type) throws RpcException;
+
+  /**
+   * Add a data compressor.
+   * 
+   * @param type
+   *          the data compressor type
+   * @param compressorClass
+   *          the data compressor class
+   */
+  public void addCompressor(int type, Class<? extends DataCompressor> compressorClass);
+
+  /**
+   * Get the factory version.
+   * 
+   * @return the factory versions
+   */
   public int version();
 }
