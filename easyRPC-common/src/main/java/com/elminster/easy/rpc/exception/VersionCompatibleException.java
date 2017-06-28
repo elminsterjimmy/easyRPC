1bc0f0dfe43ca87a7fdb08139c43806bc07ff032
Jinglei.Gu <jinglei.gu@oracle.com>
2017-06-28 19:40:33 +0800

    new Exceptions.

---
+++ easyRPC-common/src/main/java/com/elminster/easy/rpc/exception/VersionCompatibleException.java
@@ -0,0 +1,13 @@
+package com.elminster.easy.rpc.exception;
+
+public class VersionCompatibleException extends RpcException {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  public VersionCompatibleException(String string) {
+    super(string);
+  }
+}
