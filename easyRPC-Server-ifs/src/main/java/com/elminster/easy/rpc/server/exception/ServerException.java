2a61568cc2008d8202b97135de8bb3e2b8a1fce4
Jinglei.Gu <jinglei.gu@oracle.com>
2017-06-30 18:05:10 +0800

    add serial version uid.

--- easyRPC-Server-ifs/src/main/java/com/elminster/easy/rpc/server/exception/ServerException.java
+++ easyRPC-Server-ifs/src/main/java/com/elminster/easy/rpc/server/exception/ServerException.java
@@ -2,6 +2,11 @@ package com.elminster.easy.rpc.server.exception;
 
 public class ServerException extends Exception {
 
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
   public ServerException(String msg, Throwable t) {
     super(msg, t);
   }
